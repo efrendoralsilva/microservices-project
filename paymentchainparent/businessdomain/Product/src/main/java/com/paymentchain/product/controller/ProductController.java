09ad5567fd2b7269918d3c3937e4a18d3be43f40
Efrén Doral Silva <edoral@odeene.es>
2024-07-24 14:25:05 +0200

    feat: add product microservice and CRUD operations

---
+++ paymentchainparent/businessdomain/Product/src/main/java/com/paymentchain/product/controller/ProductController.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/RestController.java to edit this template
+ */
+package com.paymentchain.product.controller;
+
+import com.paymentchain.product.entities.Product;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+/**
+ *
+ * @author EfrénDoralSilva
+ */
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    
+    @GetMapping()
+    public List<Product> list() {
+        return null;
+    }
+    
+    @GetMapping("/{id}")
+    public Product get(@PathVariable String id) {
+        return null;
+    }
+    
+    @PutMapping("/{id}")
+    public ResponseEntity<?> put(@PathVariable String id, @RequestBody Product input) {
+        return null;
+    }
+    
+    @PostMapping
+    public ResponseEntity<?> post(@RequestBody Product input) {
+        return null;
+    }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable String id) {
+        return null;
+    }
+    
+}
